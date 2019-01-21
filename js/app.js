8123137732cdd9630eaa3f9faf4e4dbc5b19f025
Sam Reilly <samreilly@gmail.com>
2019-01-21 13:03:58 +1100

    Initial position for enemy and move forward if within x boundary

--- js/app.js
+++ js/app.js
@@ -1,23 +1,21 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    //set a staring point for the sprite
+    this.x=0;
+    this.y=0;
+    this.step=101;
 
-    //X and y pos?
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
-    //Need to move enemy around
+    //If the sprite is within x boundary, move forward
+    if (this.x <this.step*4){
+        this.x += 200 * dt;
+    }
     
 };
 
